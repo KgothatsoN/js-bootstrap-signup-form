09cadd33535b0c44902b3f300d5f6424fc40a4e2
KgothatsoN <kg.otha@hotmail.com>
2021-03-01 18:56:15 +0000

    Added checkbox validation

--- javascript/main.js
+++ javascript/main.js
@@ -2,23 +2,28 @@ const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const email = document.getElementById("email")
 const form = document.getElementById("form");
+const error = document.getElementById("error")
+const checkBox = document.getElementById("flexCheckDefault");
 
 form.addEventListener ("submit", (e) => {
     e.preventDefault();
-    if(fname.value == "")    {
+    if(fname.value.trim() == "")    {
         alert("Input " + fname.name + "...")
         
     }
 
-    if(lname.value =="")    {
+    if(lname.value.trim() =="")    {
         alert("Input " + lname.name + "...")
     }
 
-    if(email.value < "")    {
+    if(email.value.trim() == "")    {
         alert("Email field cannot be empty...")
     }
     else if (email.value.length < 3)    {
         alert("Email address too short...")
     }
     
+    if(checkBox.checked == false) {
+        alert("Accept Terms & Conditions.")
+    }
 })
