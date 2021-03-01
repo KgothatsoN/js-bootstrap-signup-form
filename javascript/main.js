20dea06186b1de457468882ac7df35ca127638fe
KgothatsoN <kg.otha@hotmail.com>
2021-03-01 19:58:51 +0000

    Added comments

--- javascript/main.js
+++ javascript/main.js
@@ -3,27 +3,44 @@ const lname = document.getElementById("lname");
 const email = document.getElementById("email")
 const form = document.getElementById("form");
 const error = document.getElementById("error")
+const errorEmail = document.getElementById("error-email")
+const errorTC = document.getElementById("error-T&C")
 const checkBox = document.getElementById("flexCheckDefault");
+const genderSelector = document.getElementById("gender");
 
 form.addEventListener ("submit", (e) => {
-    e.preventDefault();
+    // Prevent default action (Don't refresh page)
+    
+    //First Name Validation
     if(fname.value.trim() == "")    {
-        alert("Input " + fname.name + "...")
-        
+        e.preventDefault();
+        error.style.display = "block";
+        error.innerText = "Field cannot be empty...";
     }
 
+    //Last Name Validation
     if(lname.value.trim() =="")    {
-        alert("Input " + lname.name + "...")
+        e.preventDefault();
+        error.style.display = "block";
+        error.innerText = "Field cannot be empty...";
     }
 
+    // Email Validation
     if(email.value.trim() == "")    {
-        alert("Email field cannot be empty...")
+        e.preventDefault();
+        errorEmail.style.display = "block";
+        errorEmail.innerText = "Email field cannot be empty...";
     }
     else if (email.value.length < 3)    {
-        alert("Email address too short...")
+        e.preventDefault();
+        errorEmail.style.display = "block";
+        errorEmail.innerText = "Email Address too short...";
     }
-    
+
+    //Checkbox validation
     if(checkBox.checked == false) {
-        alert("Accept Terms & Conditions.")
+        e.preventDefault();
+        errorTC.style.display = "block";
+        errorTC.innerText = "Please accept Terms & Conditions.";
     }
 })
